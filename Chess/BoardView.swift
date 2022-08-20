5edfd15c22e7b3156b293ff1a4fb4f1f07373949
Ihor <56516292+degster55@users.noreply.github.com>
2022-08-20 13:35:05 +0300

    Initial commit

---
+++ Chess/BoardView.swift
@@ -0,0 +1,17 @@
+//
+//  BoardView.swift
+//  Chess
+//
+//  Created by Ігор Ляхович on 20.08.2022.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
