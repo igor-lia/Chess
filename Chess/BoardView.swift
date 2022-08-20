8a08c929cac401b8b0303c7d3dba5ee1c2ac4711
Ihor <56516292+degster55@users.noreply.github.com>
2022-08-20 14:34:36 +0300

    Worked with Storyboard, created BoardView, drawn chess board

--- Chess/BoardView.swift
+++ Chess/BoardView.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class BoardView: UIView {
-
+    
+    let originX : CGFloat = 20
+    let originY : CGFloat = 20
+    let cellSide : CGFloat = 37
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        drawBoard()
     }
     
-
+    func drawBoard() {
+        for row in 0..<4 {
+            for col in 0..<4 {
+                drawSquare(col: col * 2, row: row * 2, color: .white)
+                drawSquare(col: 1 + col * 2, row: row * 2, color: .black)
+                drawSquare(col: col * 2, row: 1 + row * 2, color: .black)
+                drawSquare(col: 1 + col * 2, row: 1 + row * 2, color: .white)
+            }
+        }
+    }
+    
+    func drawSquare(col: Int, row: Int, color: UIColor) {
+        let path = UIBezierPath(rect: CGRect(x: originX + CGFloat(col) * cellSide, y: originY + CGFloat(row) * cellSide, width: cellSide, height: cellSide))
+        color.setFill()
+        path.fill()
+    }
 }
